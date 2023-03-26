cd7d5ca0cce2a4a07be10b0652f67c229213ef05
ERNO1997 <ejperezv1997@gmail.com>
2023-03-26 16:50:55 -0400

    initial home design

---
+++ script.js
@@ -0,0 +1,11 @@
+let typed = new Typed(".multiple-text", {
+    strings: [
+        "Software Developer",
+        "Freelancer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
